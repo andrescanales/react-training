e8420fa80c212ecf811887726d8d0632dcaf0864
Andres Canales <andres.canales@macbook.com>
2018-11-14 15:53:46 -0600

    Adding ErrorBoundary feature to allow prod env not break the entire app but to send a message when an error is happening and when using ErrorBoundary component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,6 +3,10 @@ import './Person.css';
 
 
 const person = (props) => {
+	const rnd = Math.random();
+		if(rnd > 0.7){
+			throw new Error("Something went wrong");
+		}
 	return (
 		<div className="Person">
 			<p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!</p>
