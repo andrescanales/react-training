25473481106e68d8081079b699c6deb7e74bc605
Andres Canales <andres.canales@macbook.com>
2018-12-05 15:54:36 -0600

    Using ref(reserved word) to apply focus on an input element

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -16,6 +16,9 @@ class Person extends Component {
 
   componentDidMount(){
     console.log('[Person.js] Inside componentDidMount()');
+    if (this.props.position === 0){
+    	this.inputElement.focus();
+    }
   }
 
 	render(){
@@ -25,7 +28,11 @@ class Person extends Component {
 				<p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old!</p>
 				{/* Children element are within tags in person component */}
 				<p>{this.props.children}</p>
-				<input type="text" onChange={this.props.changed} value={this.props.name} />
+				<input 
+					ref={(inp)=>{ this.inputElement = inp}}
+					type="text" 
+					onChange={this.props.changed} 
+					value={this.props.name} />
 			</WithClass>
 		);
 	}
